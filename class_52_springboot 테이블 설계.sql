7c356b08bcba69390d2a1770b172a2ba34f967e1
dbstjlee <dbstjlee@naver.com>
2024-08-09 14:19:52 +0900

    spring boot 입출금 내역 확인 쿼리

--- class_52_springboot 테이블 설계.sql
+++ class_52_springboot 테이블 설계.sql
@@ -3,31 +3,31 @@
 -- drop table history_tb;
 -- drop table user_tb;
 
--- 데이터 베이스 생성
--- mybank
+-- 데이터 베이스 생성 
+-- mybank 
 
 create database mybank;
 use mybank;
 
--- 외래키 지정 안 함. <--- 생산성 하락 때문
--- 유저 테이블 설계
+-- 외래키 지정 안함... 
+-- 유저 테이블 설계 
 create table user_tb(
-	id int auto_increment primary key,
-    username varchar(50) not null unique,
-    password varchar(100) not null,
-    fullname varchar(50) not null,
+	id int auto_increment primary key, 
+    username varchar(50) not null unique, 
+	password varchar(100) not null, 
+	fullname varchar(50) not null, 
     created_at timestamp not null default now()
-);
+); 
 desc user_tb;
 
--- 계좌 정보 테이블
+-- 계좌 정보 테이블 
 create table account_tb(
-	id int auto_increment primary key,
+	id int auto_increment primary key, 
     number varchar(30) not null unique, 
-    password varchar(30) not null,
-    balance bigint not null comment '계좌잔액',
-    created_at timestamp not null default now(),
-    user_id int
+    password varchar(30) not null, 
+    balance bigint not null comment '계좌잔액', 
+    created_at timestamp not null default now(), 
+	user_id int 
 );
 desc account_tb;
 
@@ -44,17 +44,15 @@ desc account_tb;
 */
 create table history_tb(
 	id int auto_increment primary key comment '거래내역 ID',
-    amount bigint not null comment '거래금액',
+	amount bigint not null comment '거래금액',
     w_account_id int comment '출금 계좌 ID',
     d_account_id int comment '입금 계좌 ID',
     w_balance bigint comment '출금 요청 후 계좌 잔액',
-    d_balance bigint comment '입금 요청 후 계좌 잔액',
-    created_at timestamp not null default now()
+    d_balance bigint comment '입금 요청 후 계좌 잔액', 
+    created_at timestamp not null default now() 
 );
 desc history_tb;
 
--- 샘플 데이터 입력 
--- 유저 
 insert into user_tb(username, password, fullname, created_at)
 values('길동', '1234', '고', now());
 
@@ -63,6 +61,7 @@ values('둘리', '1234', '애기공룡', now());
 
 insert into user_tb(username, password, fullname, created_at)
 values('마이클', '1234', '콜', now());
+
 
 select * from user_tb;
 
@@ -81,32 +80,30 @@ values('3333', '1234', 0, 3, now());
 
 select * from account_tb;
 
--- 거래 내역 등록 연습
-/**
-가상 현재 잔액 
 
-1111계좌는 1000원
-2222계좌는  1000원
-3333계좌는    0원
-*/
+-- 거래 내역 등록 연습 
+-- 가상 현재 잔액 
+-- 1111계좌는 1000원 
+-- 2222계좌는 1000원 
+-- 3333계좌는    0원 
 
--- 1. 이체 내역을 기록 
--- 1111번 계좌에서 222번 계좌로 100원 이체한다. 
+-- 1, 이체 내역을 기록  
+-- 1111번 계좌에서 2222번계좌로 100원 이체한다. 
+
 insert into history_tb(amount, w_balance, d_balance, w_account_id, d_account_id, created_at)
-            value(100, 900, 1100, 1, 2, now());
+			values(100, 900, 1100, 1, 2, now());
+
+-- 2. ATM 기기에서 출금 
+-- 1111 계좌에서 100원만 출금하는 히스토리를 만들어 보세요 
+insert into history_tb(amount, w_balance, d_balance, w_account_id, d_account_id, created_at)
+			values(100, 800, null, 1, null, now());
+
+-- 3. ATM 기기에서 입금 
+-- 1111 계좌로 500원만 입금하는 히스토리를 만들어 보세요 
+insert into history_tb(amount, w_balance, d_balance, w_account_id, d_account_id, created_at)
+			values(500, null, 1300, null, 1, now());
+
 
 select * from history_tb;
 select * from account_tb;
-
--- 2. ATM 기기에서 출금
--- 1111계좌에서 100원만 출금하는 히스토리를 만들어라.
-insert into history_tb(amount, w_balance, d_balance, w_account_id, d_account_id, created_at) 
-            value(100, 800, null, 1, null, now());
-
-
--- 3. ATM 기기에서 입금
--- 1111계좌로 500원만 입금하는 히스토리를 만들어라.
-insert into history_tb(amount, w_balance, d_balance, w_account_id, d_account_id, created_at)
-            value(500, null, 1300, null, 1, now());
-
-
+select * from user_tb;
---
+++ class_53_springboot 입출금, 이체 내역 조회 쿼리.sql.sql
@@ -0,0 +1,125 @@
+-- 1단계
+
+select * from history_tb;
+select * from account_tb;
+
+-- 1번 계좌에서 출금 내역
+select * from history_tb where w_account_id = 1;
+
+-- 1번 계좌에서 입금 내역
+select * from history_tb where d_account_id = 1;
+
+
+-- 2단계
+-- 계좌 번호와 함께 출력하기 --
+-- 한 계좌에 대한 모든 출금 내역을 확인 한다면
+
+-- 1번 계좌에 대한 출금 내역 + 계좌번호 출력하기 
+select h.id, h.amount, h.w_balance, a.number, h.created_at
+from history_tb as h
+left join account_tb as a on h.w_account_id = a.id
+where h.w_account_id = 1;
+
+-- 1번 계좌에 대한 입금 내역 + 계좌번호 출력하기 
+select h.id, h.amount, h.d_balance, a.number, h.created_at
+from history_tb as h
+left join account_tb as a on h.d_account_id = a.id
+where h.d_account_id = 1;
+
+
+
+-- coalesce 사용법 및 CAST 사용법
+SELECT COALESCE(2, 1, 'third_value', 'fourth_value'); -- 2
+SELECT COALESCE(NULL, 1, 'third_value', 'fourth_value'); -- 1
+SELECT COALESCE(NULL, NULL, 'third_value', 'fourth_value'); -- third_value
+SELECT COALESCE(NULL, NULL, NULL, 'fourth_value'); -- fourth_value
+SELECT COALESCE(NULL, NULL, NULL, NULL);  -- NULL
+
+-- select CAST('123' as INT); -- 실행 안 되지만 h2-console에서는 실행됨.
+select CAST('123' as signed); -- 양수, 음수 다 포함
+select CAST(-123 as signed); -- 양수, 음수 다 포함 --> -123 출력됨
+select CAST('123' as unsigned); -- 양수만을 표현할 때 사용
+
+
+
+-- 3단계
+-- 출금이 만약 이체라면 키 값에 receiver(금액의 대상)을 출력
+-- 1번 계좌에 대한 출금 내역
+select h.id, h.amount, h.w_balance as balance, a.number, h.created_at,
+	   coalesce(CAST(h.d_account_id as char(10)), 'ATM') as receiver 
+from history_tb as h
+left join account_tb as a on h.w_account_id = a.id
+where h.w_account_id = 1;
+
+-- 1번 계좌에 대한 입금 내역
+select h.id, h.amount, h.d_balance as balance, a.number, h.created_at,
+	coalesce(CAST(h.w_account_id as char(10)), 'ATM') as sender
+from history_tb as h
+left join account_tb as a on h.d_account_id = a.id
+where h.d_account_id = 1;
+
+
+-- desc history_tb; -> w_account_id와 d_account_id는 int지만 char로 형변환(CAST) 함.
+desc history_tb;
+
+-- 4단계
+-- 1번 계좌에 대한 입출금, 이체 전체 내역 확인
+select h.id, h.amount,
+	case
+		when h.w_account_id = 1 then(h.w_balance)
+		when h.d_account_id = 1 then(h.d_balance)
+	end as balance,
+    coalesce(CAST(wa.number as char(10)), 'ATM') as sender,
+    coalesce(CAST(da.number as char(10)), 'ATM') as receiver,
+    h.created_at
+from history_tb as h
+left join account_tb as wa on h.w_account_id = wa.id
+left join account_tb as da on h.d_account_id = da.id
+where h.w_account_id =  1 or h.d_account_id = 1;
+
+
+-- when h.w_account_id = 1 then(h.w_balance)
+-- > 출금 id가 1번일 때 출력 후 잔액을 출력해야 한다. (출금)
+
+-- when h.d_account_id = 1 then(h.d_balance)
+-- > 입금 id가 1번일 때 입금 후 잔액을 출력해야 한다. (입금)
+
+
+
+
+
+
+-- 코드 상에서 사용할 쿼리 생성
+
+-- 출금에 대한 쿼리 출력
+-- receiver : 금액을 받는 대상,
+-- 기능적으로 하나의 JSP 페이지에서 전체 쿼리에 대한 결과 집합의 컬럼명을 
+-- 동일하게 사용할 수 있도록 쿼리를 수정한다. (같은 Model Class에 담을 예정)
+
+-- 출금 
+-- 1111 --> ATM 출금, 1111 ---> 2222 이체
+
+select h.id, h.amount, h.w_balance AS balance, h.created_at,
+		coalesce(CAST(da.number as char(10)), 'ATM') as receiver,
+        wa.number as sender
+from history_tb as h
+left join account_tb as wa on wa.id = h.w_account_id
+left join account_tb as da on da.id = h.d_account_id
+where h.w_account_id = 1;
+
+-- da.number, d_account_id == null(ATM이 되어야 함)(받은 이)
+
+
+-- 입금에 대한 쿼리 출력
+-- ATM --> 1111 계좌로 입금, 다른 계좌 --> 1111 계좌로 입금
+select h.id, h.amount, h.d_balance AS balance, h.created_at,
+		coalesce(CAST(wa.number as char(10)), 'ATM') as sender,
+        da.number as receiver
+from history_tb as h
+left join account_tb as wa on wa.id = h.w_account_id
+left join account_tb as da on da.id = h.d_account_id
+where h.d_account_id = 1;
+
+-- wa.number, w_account_id == null(ATM이 되어야 함)(보낸이)
+
+
