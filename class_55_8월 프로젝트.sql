a474b15457463f3edecf1462165e7f2e6c5b262c
dbstjlee <dbstjlee@naver.com>
2024-09-06 18:57:13 +0900

    final project 쿼리문 test 중

--- class_55_8월 프로젝트.sql
+++ class_55_8월 프로젝트.sql
@@ -32,9 +32,9 @@ create table notice_tb(
     created_at timestamp default now() not null,
     views int default 0 not null
 );
-drop table notice_tb;
+-- drop table notice_tb;
 desc notice_tb;
-insert into notice_tb(categories, title, content, created_at) values('test1', 'test1', 'test1', now()),
+insert into notice_tb(categories, title, content, created_at) values('[업데이트]', '제목1', '내용1', now()),
 																						('test2', 'test2', 'test2', now()),
                                                                                         ('test3', 'test3', 'test3', now()),
                                                                                         ('test3', 'test3', 'test3', now()),
@@ -46,9 +46,16 @@ insert into notice_tb(categories, title, content, created_at) values('test1', 't
                                                                                         ('test3', 'test3', 'test3', now());
 														
                                                         
-insert into notice_tb(categories, title, content, created_at) values('test1', 'test1', 'test1', now());                                           
+insert into notice_tb(categories, title, content, created_at) values('[업데이트]', '제목1', '내용1', now());                                           
+insert into notice_tb(categories, title, content, created_at) values('[업데이트]', '제목2', '내용2', now());                                           
+insert into notice_tb(categories, title, content, created_at) values('[구독/결제]', '제목1', '내용1', now());                                           
+insert into notice_tb(categories, title, content, created_at) values('[구독/결제]', '제목2', '내용2', now());                                           
+insert into notice_tb(categories, title, content, created_at) values('[회원]', '제목1', '내용1', now());                                           
+insert into notice_tb(categories, title, content, created_at) values('[회원]', '제목2', '내용2', now());    
+
+                                       
 insert into notice_tb(categories, title, content, created_at) values('1', '1', '1', now());                                           
-                                                                    
+desc notice_tb;
 SELECT * FROM notice_tb;
 UPDATE notice_tb SET categories = 'test4', title = 'test4', content = 'test4' where id = 3;
 DELETE FROM notice_tb WHERE id = 3;
@@ -71,9 +78,17 @@ SELECT COUNT(*) AS count FROM notice_tb WHERE title AND content LIKE '1' ORDER B
 SELECT * FROM notice_tb WHERE title LIKE '1' ORDER BY id DESC limit 4 offset 0;
 SELECT COUNT(*) AS count WHERE title LIKE '1' ORDER BY id DESC limit 4 offset 0;
 
+select * from user_withdraw_tb;
 
-SELECT count(*) AS count FROM notice_tb WHERE title AND content LIKE ? ;
 
+SELECT * FROM notice_tb WHERE title LIKE '%1%' ORDER BY id DESC limit 10 offset 0;
+SELECT * FROM notice_tb WHERE title LIKE '%2%' OR content LIKE '%2%' ORDER BY id DESC limit 10 offset 0;
+SELECT * FROM notice_tb WHERE title LIKE '%2%' AND content LIKE '%2%' ORDER BY id DESC limit 10 offset 0;
+SELECT * FROM notice_tb WHERE categories LIKE '%1%' ORDER BY id DESC limit 10 offset 0;
+
+SELECT IFNULL(COUNT(*), 0) AS count FROM notice_tb WHERE title LIKE '%1%';
+
+SELECT * FROM notice_tb;
 
 
 
@@ -90,10 +105,40 @@ create table admin_tb(
     created_at timestamp default now() not null
 );
 desc admin_tb;
-drop table admin_tb;
+-- drop table admin_tb;
 
 SELECT * FROM admin_tb;
 SELECT * FROM admin_tb WHERE admin_id = "admin";
 
 INSERT INTO admin_tb(admin_name, admin_id, admin_password, admin_email, admin_gender, created_at) 
 VALUES('admin', 'admin', 'admin', 'admin@naver.com', '남성', now());
+
+
+update admin_tb set admin_password = '$2a$10$flHqm7OtGC644Qa0l3Bn6OA5OFBTQ2H/e9IwsakR0Sh5Y8RUC90W6';
+update admin_tb set admin_password = 'admin' WHERE admin_id = 'admin';
+select * from admin_tb where admin_id = 'admin'; 
+
+
+
+-- ------------------------------------------------------------
+-- 관리자 대시보드
+
+-- 전체 사용자의 수
+SELECT COUNT(*) FROM user_tb;
+SELECT * FROM user_tb;
+desc user_tb;
+
+-- 전체 사용자 수(월별)
+SELECT YEAR(created_at) AS year, MONTH(created_at) AS month, COUNT(*) AS userCount
+FROM user_tb
+GROUP BY YEAR(created_at), MONTH(created_at)
+ORDER BY year, month;
+
+
+
+
+
+
+
+
+
